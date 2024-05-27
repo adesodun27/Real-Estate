db90085472b76cec3faf89ad12a908241cabb6e0
Adesodun Titilope Temiloluwa <115459326+adesodun27@users.noreply.github.com>
2024-05-27 17:48:19 +0100

    Initial commit

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./getStarted.css";
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexCenter inner-container">
+                  <span className="primaryText"> Get Started with Homyz</span>
+                  <span className="SecondaryText">
+            Subscribe and find super attraction price quotes from us <br />
+            Find your residence soon
+                  </span>
+                  <button className="button">
+                      <a href="mailto:adesoduntitilope@gmail.com">Get Started</a>
+                  </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetStarted;
