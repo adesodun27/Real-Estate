b4fdf8ecac7dbcb39e855bf7b9f93805a4970ad7
Adesodun Titilope Temiloluwa <115459326+adesodun27@users.noreply.github.com>
2024-06-18 17:44:51 +0100

    Second commit

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -10,7 +10,6 @@ import {
 } from "react-accessible-accordion";
 import "react-accessible-accordion/dist/fancy-example.css";
 import { MdOutlineArrowDropDown } from "react-icons/md";
-import "./value.css";
 import data from "../../utils/accordion";
 
 const Value = () => {
